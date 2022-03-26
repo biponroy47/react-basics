8e6faa22f1ed92c315aeda03e3ebb7a6734e9920
Bipon Roy <68929730+biponroy47@users.noreply.github.com>
2022-03-25 23:24:57 -0400

    Uploading test files

---
+++ src/books.js
@@ -0,0 +1,21 @@
+export const books = [
+  {
+    id: 1,
+    img: "https://m.media-amazon.com/images/I/51uE7czRB1L._AC_UL320_.jpg",
+    title: "The List",
+    author: "Siobhan Vivian",
+  },
+
+  {
+    id: 2,
+    img: "https://m.media-amazon.com/images/I/81JeFTlTjcL._AC_UL480_FMwebp_QL65_.jpg",
+    title: "The Silent Wife",
+    author: "Kerry Fisher",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51-uspgqWIL._SX329_BO1,204,203,200_.jpg",
+    title: "Atomic Habits",
+    author: "James Clear",
+  },
+];
